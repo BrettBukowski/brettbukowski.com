9290f734e599e5a6bfc99fa44326f92b8d51eeba
Brett Bukowski <brett.bukowski@gmail.com>
2012-12-17 07:23:54 -0700

    Initial set of stuff

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,66 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+!function(exports) {
+
+    var emptyConstructor = function() {};
+
+function mix(receiver, provider) {
+    for (key in provider) {
+        receiver[key] = provider[key];
+    }
+
+    return receiver;
+}
+
+function inherit(parent, props, statics) {
+    var child = (props && props.hasOwnProperty('constructor'))
+        ? props.constructor
+        : function() { parent.apply(this, arguments); };
+
+    mix(child, parent);
+
+    emptyConstructor.prototype = parent.prototype;
+    child.constructor = new emptyConstructor();
+
+    if (props) {
+        mix(child.prototype, props);
+    }
+    if (statics) {
+        mix(child, statics);
+    }
+
+    child.prototype.constructor = child;
+    child.__super__ = parent.prototype;
+
+    return child;
+}
+
+function extend(props, statics) {
+    var child = inherit(this, props, statics);
+    child.extend = this.extend;
+
+    return child;
+}
+
+var slice = Array.prototype.slice;
+
+function bind(func, context) {
+    if (func.bind === Function.prototype.bind) {
+        return Function.prototype.bind.apply(func, slice.call(arguments, 1));
+    }
+
+    var args = slice.call(arguments, 2);
+    return function() {
+        return func.apply(context, args.concat(slice.call(arguments)));
+    };
+}
+
+var Component = function() {};
+Component.extend = extend;
+Component.bind = bind;
+
+exports.Component = Component;
+
+}(window);
