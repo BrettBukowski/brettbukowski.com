4c7470ea1535b3ef28efe05f5ea1f440c90234c7
Brett Bukowski <brett.bukowski@gmail.com>
2012-12-30 13:32:22 -0700

    Capistrano deploy stuff

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+set :application, "brettbukowski"
+set :repository,  "git://brettbukowski@bitbucket.org/brettbukowski/brettbukowski.com.git"
+set :branch, "master"
+set :scm, :git
+
+server "brettbukowski.com", :app, :web
+
+set :user, 'brett'
+set :deploy_to, '/var/brettbukowski'
+set :use_sudo, true
+set :deploy_via, :copy
+set :copy_strategy, :export
+default_run_options[:pty] = true
+
+namespace :deploy do
+  task :start, :roles => [:web, :app] do
+    run "cd #{deploy_to}/current && nohup thin -e production -C config/thin.yml -R config.ru start"
+  end
+
+  task :stop, :roles => [:web, :app] do
+    run "cd #{deploy_to}/current && nohup thin -e production -C config/thin.yml -R config.ru stop"
+  end
+
+  task :restart, :roles => [:web, :app] do
+    deploy.stop
+    deploy.start
+  end
+
+  task :cold do
+    deploy.update
+    deploy.start
+  end
+end
+
+namespace :brett do
+  desc "Compile assets"
+  task :compile_assets do
+    run "cd #{deploy_to}/current; bundle exec rake assets:compile"
+  end
+end
+
+after "deploy:create_symlink", "brett:compile_assets"
+after "deploy:restart", "deploy:cleanup"
