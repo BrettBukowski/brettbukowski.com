14d9a152dee9567e519d6eff0aedc3295a8da24e
Brett Bukowski <brett.bukowski@gmail.com>
2014-02-15 15:35:00 -0700

    Remove contact form in favor of a simple mailto link

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ end
 
 class RootController < App
   get '/' do
-    erb :index
+    File.read(File.join('public', 'index.html'))
   end
 
   get '/resume' do
@@ -25,39 +25,39 @@ class RootController < App
     File.read settings.views + '/resume.md'
   end
 
-  post '/contact' do
-    if request.form_data?
-      given = Rack::Utils.parse_nested_query(request.body.read)
-      email = given['email']
-      message = given['message']
+#   post '/contact' do
+#     if request.form_data?
+#       given = Rack::Utils.parse_nested_query(request.body.read)
+#       email = given['email']
+#       message = given['message']
 
-      message.strip!
-      email.strip!
+#       message.strip!
+#       email.strip!
 
-      if !email.empty? && email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/ && !message.empty?
-        message = <<MESSAGE
-From: #{email} <#{email}>
+#       if !email.empty? && email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/ && !message.empty?
+#         message = <<MESSAGE
+# From: #{email} <#{email}>
 
-#{message}
-MESSAGE
-        require 'json'
+# #{message}
+# MESSAGE
+#         require 'json'
 
-        system "mail", "-s", "Message Received From Contact Form", "brett.bukowski@gmail.com", message
+#         system "mail", "-s", "Message Received From Contact Form", "brett.bukowski@gmail.com", message
 
-        if request.preferred_type == 'application/json'
-          {:sent => true}.to_json
-        else
-          erb :index, :locals => {:message => "Thanks for getting in touch! I'll respond presently."}
-        end
-      else
-        if request.preferred_type == 'application/json'
-          {:sent => false}.to_json
-        else
-          erb :index, :locals => {:message => "There was a problem. Check the form and please try again."}
-        end
-      end
-    end
-  end
+#         if request.preferred_type == 'application/json'
+#           {:sent => true}.to_json
+#         else
+#           erb :index, :locals => {:message => "Thanks for getting in touch! I'll respond presently."}
+#         end
+#       else
+#         if request.preferred_type == 'application/json'
+#           {:sent => false}.to_json
+#         else
+#           erb :index, :locals => {:message => "There was a problem. Check the form and please try again."}
+#         end
+#       end
+#     end
+#   end
 
   not_found do
     send_file File.join(settings.public_folder, '404.html')
