a2642030c1f28be12468818f5ad6a8a2a521c868
Brett Bukowski <brett.bukowski@gmail.com>
2014-05-15 22:22:46 -0600

    Add some flair

--- app/js/application.js
+++ app/js/application.js
@@ -1,2 +1,23 @@
 //= require plugins.js
 //= require retina.js
+
+/* global $,WOW*/
+$(function () {
+  $('[data-scroll]').click(function (e) {
+    e.preventDefault();
+
+    var target = e.currentTarget,
+        dest = target.getAttribute('data-scroll') == 'top' ? 'body' : target.getAttribute('href');
+
+    $('body').animate({ scrollTop: $(dest).offset().top }, 1000, function () {
+      if (dest.indexOf('#') != 0) {
+        dest = '';
+      }
+      window.location.hash = dest;
+    });
+  });
+
+  new WOW().init();
+
+  $.stellar({ responsive: true });
+});
