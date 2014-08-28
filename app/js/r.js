2c2ce40fbf83faf592d678902d64c2c61305ed9f
Brett Bukowski <brett.bukowski@gmail.com>
2014-08-27 21:42:54 -0600

    Remove jQuery dependency on resume

--- app/js/r.js
+++ app/js/r.js
@@ -1,18 +1,56 @@
-/* global $*/
-$(function() {
-  (function supplyTooltips () {
+(function () {
+  'use strict';
 
-    $('[title]').each(function () {
-      var el = $(this);
-      el.after('<div class="what">?</div><div class="tooltip" style="display:none">' + el.attr('title') + '</div>');
-      el.removeAttr('title');
-    });
+  Array.from = Array.from || function (arrayLike, mapFn, context) {
+    var array = Array.prototype.slice.call(arrayLike);
 
-    function fadeToggle () {
-      $(this).next().fadeToggle();
+    if (typeof mapFn == 'function') {
+      var mapped = [];
+      for (var i = 0, len = array.length; i < len; i++) {
+        mapped.push(mapFn.call(context || null, array[i], i, array));
+      }
+
+      return mapped;
     }
 
-    $('.what').hover(fadeToggle, fadeToggle);
+    return array;
+  };
 
-  })();
-});
+  function createEl (name, attributes) {
+    var el = document.createElement(name);
+
+    for (var i in attributes) {
+      if (attributes.hasOwnProperty(i)) {
+        el[i] = attributes[i];
+      }
+    }
+
+    return el;
+  }
+
+  function mouseHandler (e) {
+    e.target.nextSibling.classList.toggle('visible');
+  }
+
+  Array.from(document.querySelectorAll('[title]'), function (el) {
+    var indicator = createEl('div', {
+      'innerHTML': '?',
+      'className': 'what'
+    });
+
+    el.parentNode.insertBefore(indicator, el.nextSibling);
+
+    var tooltip = createEl('div', {
+      'className': 'tooltip',
+      'innerHTML': el.getAttribute('title')
+    });
+
+    indicator.parentNode.insertBefore(tooltip, indicator.nextSibling);
+
+    el.removeAttribute('title');
+
+    indicator.addEventListener('mouseout', mouseHandler);
+    indicator.addEventListener('mouseover', mouseHandler);
+  });
+
+})();
