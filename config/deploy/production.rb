c45826f81c415353e717c03d2d2c1f69cc3ea1b3
Brett Bukowski <brett.bukowski@gmail.com>
2024-06-13 21:34:59 -0700

    Replace capistrano app with a simple static html page

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-set :stage, :production
-server 'gato', roles: %w{web app}
-set :ssh_options, {
-  user: 'brett',
-  keys: %w(/Users/brettbukowski/.ssh/id_rsa),
-  forward_agent: true,
-  auth_methods: %w(publickey password)
-}
